9935fb65553237eb410387be70bbd8acec5ccab6
Piotr Moszkowicz <piotr@moszkowicz.pl>
2019-09-18 17:09:35 +0200

    Add wallet module

---
+++ src/wallet/wallet.entity.ts
@@ -0,0 +1,24 @@
+import {
+  AutoIncrement,
+  Column,
+  Length,
+  Model,
+  PrimaryKey,
+  Table,
+  Unique,
+} from "sequelize-typescript";
+
+@Table({
+  underscored: true,
+})
+export class Wallet extends Model<Wallet> {
+  @AutoIncrement
+  @PrimaryKey
+  @Column
+  public id: number;
+
+  @Unique
+  @Length({ min: 35, max: 35 })
+  @Column
+  public address: string;
+}

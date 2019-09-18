fbdf78b96551b6d6443f0250991a7728e6cd7cbd
Piotr Moszkowicz <piotr@moszkowicz.pl>
2019-09-18 17:32:46 +0200

    Add create method to wallet service

--- src/wallet/wallet.service.ts
+++ src/wallet/wallet.service.ts
@@ -1,5 +1,6 @@
 import { Inject, Injectable } from "@nestjs/common";
 import { Wallet } from "./wallet.entity";
+import { CreateWalletDto } from "./dto/create-wallet.dto";
 
 @Injectable()
 export class WalletService {
@@ -7,6 +8,14 @@ export class WalletService {
     @Inject("WALLET_REPOSITORY")
     private readonly walletRepository: typeof Wallet,
   ) {}
+
+  async create(createWalletDto: CreateWalletDto): Promise<Wallet> {
+    const wallet = new Wallet();
+    wallet.address = createWalletDto.address;
+    wallet.webhookUrl = createWalletDto.webhookUrl;
+
+    return wallet.save();
+  }
 
   async findAll(): Promise<Wallet[]> {
     return this.walletRepository.findAll<Wallet>();

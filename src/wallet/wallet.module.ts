99f55a46048d1ddb4954eae0c444f3c3a1278c30
Piotr Moszkowicz <piotr@moszkowicz.pl>
2019-09-18 17:33:22 +0200

    Fix wallet module

--- src/wallet/wallet.module.ts
+++ src/wallet/wallet.module.ts
@@ -1,9 +1,14 @@
 import { Module } from "@nestjs/common";
+
 import { DatabaseModule } from "../database/database.module";
+
+import { WalletController } from "./wallet.controller";
+import { WalletService } from "./wallet.service";
 import { walletProviders } from "./wallet.providers";
 
 @Module({
   imports: [DatabaseModule],
-  providers: [...walletProviders],
+  controllers: [WalletController],
+  providers: [WalletService, ...walletProviders],
 })
 export class WalletModule {}
